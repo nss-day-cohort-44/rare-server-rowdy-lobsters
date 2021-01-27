ce54e555047eb754928c6d2ce1fcaf40f634b024
billwishop <willbishop.hello@gmail.com>
2021-01-27 13:52:58 -0600

    Typo 2

--- request_handler.py
+++ request_handler.py
@@ -122,7 +122,7 @@ class HandleRequests(BaseHTTPRequestHandler):
             new_item = create_tags(post_body)
         
         # Encode the item and send in repsonse
-        self.wfile.write(f"{new_item".encode())
+        self.wfile.write(f"{new_item}".encode())
 
 def main():
     host = ''
